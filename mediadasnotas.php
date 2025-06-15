4a1a8bf7802b1f8d2489e2c086a2ec58ac97fee8
maxthewizardd <115903658+maxclyt@users.noreply.github.com>
2025-06-14 21:07:49 -0400

    Update mediadasnotas.php

--- mediadasnotas.php
+++ mediadasnotas.php
@@ -1,5 +1,4 @@
 <?php
-     <?php
     $m = ((($_POST["nt1"]*$_POST["pt1"])+($_POST["nt2"]*$_POST["pt2"])+($_POST["nt3"]*$_POST["pt3"]))/($_POST["pt1"]+$_POST["pt2"]+$_POST["pt3"]));
     echo "<h1>Sua Media:</h1>"."<h1>$m</h1>";
     if ($m >= 6){

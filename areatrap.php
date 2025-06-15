84997a9b10fc38e86dd2eb08edaf35b6b7837343
maxthewizardd <115903658+maxclyt@users.noreply.github.com>
2025-06-14 20:54:57 -0400

    Add files via upload

---
+++ areatrap.php
@@ -0,0 +1,13 @@
+<?php
+
+$b1 = $_POST["bm"];
+$b2 = $_POST["bmn"];
+$h = $_POST["ht"];
+
+$a = (($b1 + $b2) * $h)/2;
+
+echo "<h1>A area do trapezio:</h1>"."<h1>$a</h1>";
+
+
+
+?>
---
+++ volumeesfera.php
@@ -0,0 +1,9 @@
+<?php
+$r = $_POST["r1"];
+$pi = 3.14;
+
+$v = (4/3) * $pi * ($r*$r*$r);
+
+
+echo "<h1>O volume da esfera:</h1>"."<h1>$v</h1>";
+?>
